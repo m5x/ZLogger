6e00b2e1e0ebe2ce5402a8a279e08e6b2c68a6e5
Martin Dvořák <m5x@9ll.cz>
2025-06-15 03:49:00 +0200

    cosmetics

--- src/ZLogger/LogStates/StringFormatterLogState.cs
+++ src/ZLogger/LogStates/StringFormatterLogState.cs
@@ -2,7 +2,6 @@ using System.Buffers;
 using System.Diagnostics.CodeAnalysis;
 using System.Text;
 using System.Text.Json;
-using ZLogger.Formatters;
 using ZLogger.Internal;
 
 namespace ZLogger.LogStates
